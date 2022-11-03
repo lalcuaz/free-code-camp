78722bd2871f377322021b6d054a9ce6f2eecbc3
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-11-03 16:02:22 -0400

    completed ex13

---
+++ js-algorithms-and-data-structures/06-basic-algorithm-scripting/13-falsy-bouncer.js
@@ -0,0 +1,11 @@
+function bouncer(arr) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
