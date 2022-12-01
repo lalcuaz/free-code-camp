70cf50bf85d9d7cd746ebadd19a4f456853bbee4
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-01 15:02:58 -0500

    completed ex12

--- js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/12-sum-all-odd-fibonacci-numbers.js
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/12-sum-all-odd-fibonacci-numbers.js
@@ -1,5 +1,45 @@
+/*       given a pos int num, return the sum of all odd fibonacci numbers
+           that are less than or equal to num
+           the first two numbers in a sequence are 1 and 1.
+           every additional number in the sequence is the sum of the two previous numbers. so first 6: 1, 1, 2, 3, 5, 8
+           sumFibs(10) returns 10, all odd fib numbs less than or equal to 10
+           are 1, 1, 3, 5        
+          const getPrevious = (arr) => {
+          const tempArray = [...arr];
+          return tempArray.pop();
+        };        
+          for (let i = 1; i <= num; ) {
+          if (i % 2 !== 0) {
+            total = total + i;
+          }
+          previous = i;
+          i = getPrevious(fibArray) + i;
+          fibArray.push(previous);
+
+          2.
+
+          for (let i = 1; i <= num; ) {
+          if (i % 2 !== 0) {
+            total += i;
+          }
+          i = previous + i;
+          previous = i - previous;
+        }
+        }*/
 function sumFibs(num) {
-  return num;
+  let previous = 1;
+  let total = 1;
+  let current = 1;
+
+  while (current <= num) {
+    if (current % 2 !== 0) {
+      total += current;
+    }
+    current += previous;
+    previous = current - previous;
+  }
+
+  return total;
 }
 
-sumFibs(4);
+console.log(sumFibs(4));
