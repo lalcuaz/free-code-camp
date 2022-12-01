c09cb8a1c770e7110dd9cebc4630ffd4000f644f
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-01 01:49:28 -0500

    competed ex 11

--- js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/11-convert-html-entities.js
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/11-convert-html-entities.js
@@ -1,5 +1,21 @@
-function convertHTML(str) {
-  return str;
-}
+/*Convert the characters &, <, >, " (double quote), and '   (apostrophe),
+      in a string to their corresponding HTML entities. */
+/*       return str
+        .replaceAll(/&/g, "&amp;")
+        .replaceAll(/</g, "&lt;")
+        .replaceAll(/>/g, "&gt")
+        .replaceAll(/"/g, "&quot;")
+        .replaceAll(/'/g, "&apos"); */
 
-convertHTML("Dolce & Gabbana");
+function convertHTML(str) {
+  const entities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+  return str.replace(/[&<>\"']/g, (match) => entities[match]);
+}
+// Dolce &amp; Gabbana
+console.log(convertHTML("Dolce & Gabbana"));
