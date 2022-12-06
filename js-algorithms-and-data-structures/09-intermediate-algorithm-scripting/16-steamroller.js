b7e5389a89a4171046e194198e150dc2f6c41e0c
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-05 21:09:09 -0500

    completed ex16

--- js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/16-steamroller.js
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/16-steamroller.js
@@ -1,5 +1,20 @@
-function steamrollArray(arr) {
-  return arr;
+/* return arr.map((num) => {
+  if (Array.isArray(num)) {
+    steamrollArray(num);
+  } else {
+    return num;
+  }
+}); */
+function steamrollArray(arr, emptyArr = []) {
+  arr.forEach((element) => {
+    if (Array.isArray(element)) {
+      steamrollArray(element, emptyArr);
+    } else {
+      emptyArr.push(element);
+    }
+  });
+  return emptyArr;
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+//[1,2,3,4]
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
