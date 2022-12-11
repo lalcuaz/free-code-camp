ad516e886db987ff7e465df4cb8a7055cd8bfae7
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-10 23:05:55 -0500

    completed ex19

--- js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/19-arguments-optional.js
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/19-arguments-optional.js
@@ -1,5 +1,44 @@
-function addTogether() {
-  return false;
+// sums two args together
+// if only one arg, return a func that expects one arg and returns the sum
+// func sumTwoAnd(oneNum)
+// return undefined if param isn't a valid number
+/* if (
+          typeof arguments[0] === "number" &&
+          typeof arguments[1] === "number"
+        ) {
+          switch (arguments.length) {
+            case 1:
+              return (num) => first + num;
+              break;
+            case 2:
+              console.log(first);
+              console.log(second);
+              return first + second;
+            default:
+              return undefined;
+          }
+        } else {
+          return undefined;
+        } */
+function addTogether(...args) {
+  const [first, second] = args;
+  if (arguments.length === 1 && typeof first === "number") {
+    return (num) => {
+      if (typeof num === "number") {
+        return num + first;
+      } else {
+        return undefined;
+      }
+    };
+  }
+
+  if (typeof first === "number" && typeof second === "number") {
+    return first + second;
+  } else {
+    return undefined;
+  }
 }
 
-addTogether(2, 3);
+console.log(addTogether(2, 3));
+console.log(addTogether(2, "3"));
+console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
---
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/20-make-a-person.js
@@ -0,0 +1,11 @@
+const Person = function (firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFullName = function () {
+    return "";
+  };
+  return firstAndLast;
+};
+
+const bob = new Person("Bob Ross");
+bob.getFullName();
