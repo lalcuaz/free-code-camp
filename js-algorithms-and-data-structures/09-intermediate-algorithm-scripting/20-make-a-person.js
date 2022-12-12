aadaeade460b894492855db271f1b52da5c9eb49
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-11 21:41:58 -0500

    completed ex20

--- js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/20-make-a-person.js
+++ js-algorithms-and-data-structures/09-intermediate-algorithm-scripting/20-make-a-person.js
@@ -1,11 +1,38 @@
 const Person = function (firstAndLast) {
   // Only change code below this line
   // Complete the method below and implement the others similarly
-  this.getFullName = function () {
-    return "";
+  let fullName = firstAndLast;
+
+  this.getFirstName = function () {
+    return fullName.split(" ")[0];
   };
-  return firstAndLast;
+
+  this.getLastName = function () {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function () {
+    return fullName;
+  };
+
+  this.setFullName = function (input) {
+    fullName = input;
+  };
+
+  this.setFirstName = function (input) {
+    fullName = input + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function (input) {
+    fullName = fullName.split(" ")[0] + " " + input;
+  };
 };
 
 const bob = new Person("Bob Ross");
-bob.getFullName();
+bob.setFirstName("Haskell");
+console.log(bob.getFullName());
+bob.setLastName("Curry");
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
+console.log(bob.getLastName());
+console.log(bob.fullName);
