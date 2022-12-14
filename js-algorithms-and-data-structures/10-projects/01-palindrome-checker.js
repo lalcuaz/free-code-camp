e9127f2d0f3820e67867cd50c77a72e2c8253586
Luigi Alcuaz <alcuazluigi@gmail.com>
2022-12-13 22:04:53 -0500

    completed 01-palindrome

--- js-algorithms-and-data-structures/10-projects/01-palindrome-checker.js
+++ js-algorithms-and-data-structures/10-projects/01-palindrome-checker.js
@@ -1,5 +1,26 @@
+//return true if palindrome
+//p is a word or sentence thats spelled the
+//same way forwards/backward
+//ignoring punct, case, spacing
+//need to remove all non-alphanumeric chars
+//everything lowercase
+// const splitStr = str.replace(/[^a-zA-Z]+\s/, "");
 function palindrome(str) {
+  const splitStr = str.split(/[^a-zA-Z0-9]+/);
+
+  let newStr = "";
+
+  for (const element of splitStr) {
+    newStr += element;
+  }
+  newStr = newStr.toLowerCase();
+  for (let i = 0, x = newStr.length - 1; i < x; i++) {
+    if (newStr[i] != newStr[x]) {
+      return false;
+    }
+    x -= 1;
+  }
   return true;
 }
 
-palindrome("eye");
+console.log(palindrome("1 eye for of 1 eye."));
