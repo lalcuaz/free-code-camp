52172d56f9bcfbc06918832027350b8b4f899159
luigialcuaz <alcuazluigi@gmail.com>
2022-12-31 01:56:58 -0800

    completed ex21

--- 03-front-end-development-libraries/04-react/21-create-a-stateful-component.jsx
+++ 03-front-end-development-libraries/04-react/21-create-a-stateful-component.jsx
@@ -2,7 +2,7 @@ class StatefulComponent extends React.Component {
   constructor(props) {
     super(props);
     // Only change code below this line
-
+    this.state = { firstName: "Luigi" };
     // Only change code above this line
   }
   render() {
